cae3557fdc234e2e4e4ca914b7272f814bf5723b
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-05 12:20:40 -0500

    change variable name

--- src/js/connector.js
+++ src/js/connector.js
@@ -81,7 +81,7 @@ window.TrelloPowerUp.initialize(
               var b = await fetch(aa).then(data=>{data})
 
               var aa = await fetch('https://api.trello.com/1/boards/62b9d5d4cd6b7c794607ebe2/cards?customFieldItems=true&key=8567e52ef0a5c3a9a4a76eb2722ea6d0&token=491a487812cc3fb1d1f188f9e693340640d287f36ef0bac4880c1fb707edbedc')
-	            var cd = await cardData.text();
+	            var cd = await aa.text();
               console.log(cd)
               
             }())
