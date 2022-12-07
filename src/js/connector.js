b30773ce805c0224c45150704f33a01a96333ae7
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-07 13:56:05 -0500

    working on flushing out board id

--- src/js/connector.js
+++ src/js/connector.js
@@ -21,63 +21,64 @@ window.TrelloPowerUp.initialize(
               const fetchPriorityData = 'https://dr2d89rv2e.execute-api.us-east-1.amazonaws.com/latest/priorityData'
               const fetchCardData = 'https://dr2d89rv2e.execute-api.us-east-1.amazonaws.com/latest/cardData'
               const createSheet = 'https://dr2d89rv2e.execute-api.us-east-1.amazonaws.com/latest/createSheet'
+              console.log(boardId)
 
               const cm = await fetch(fetchCardMembers,{method:'POST',body:boardId})
               console.log(cm)
               let memberData = await cm.text();
               console.log(memberData)
-              const cl = await fetch(fetchCardLists,{method:'POST',body:boardId})
-              let listData = await cl.text();
-              const ld = await fetch(fetchLabelData,{method:'POST',body:boardId})
-              let labelData = await ld.text();
-              const pd = await fetch(fetchPriorityData,{method:'POST',body:boardId})
-              let priorityData = await pd.text();
-              const cd = await fetch(fetchCardData,{method:'POST',body:boardId})
-              let cardData = await cd.text();
+              // const cl = await fetch(fetchCardLists,{method:'POST',body:boardId})
+              // let listData = await cl.text();
+              // const ld = await fetch(fetchLabelData,{method:'POST',body:boardId})
+              // let labelData = await ld.text();
+              // const pd = await fetch(fetchPriorityData,{method:'POST',body:boardId})
+              // let priorityData = await pd.text();
+              // const cd = await fetch(fetchCardData,{method:'POST',body:boardId})
+              // let cardData = await cd.text();
 
-              memberData = JSON.parse(memberData);
-              listData = JSON.parse(listData);
-              labelData = JSON.parse(labelData);
-              priorityData = JSON.parse(priorityData);
-              cardData = JSON.parse(cardData);
+              // memberData = JSON.parse(memberData);
+              // listData = JSON.parse(listData);
+              // labelData = JSON.parse(labelData);
+              // priorityData = JSON.parse(priorityData);
+              // cardData = JSON.parse(cardData);
                             
-              cardData.map(x=>{
-                const cardDict = {};
-                cardDict['id'] = x.id;
-                cardDict['title'] = x.name;
-                cardDict['description'] = x.desc;
-                cardDict['url'] = x.url;
-                cardDict['comments'] = x.badges.comments;
-                cardDict['list'] = listData[x.idList];
-                x.idMembers.forEach((m,index)=>{
-                  x.idMembers[index] = memberData[m]
-                })
-                cardDict['members'] = x.idMembers.join();
-                x.idLabels.forEach((l,index)=>{
-                  x.idLabels[index] = labelData[l];
-                })
-                cardDict['labels'] = x.idLabels.join(', ');
-                if(x.dateLastActivity){
-                  let date = new Date(x.dateLastActivity);
-                  date = date.toDateString();
-                  cardDict['last activity'] = date;
-                }
-                if(x.due){
-                  let date = new Date(x.due);
-                  date = date.toDateString();
-                  cardDict['due date'] = date;
-                }
-                if(x.customFieldItems.length > 0){
-                  cardDict['priority'] = priorityData[x.customFieldItems[0].idValue]
-                }
-                cardList.push(cardDict);
-              })
+              // cardData.map(x=>{
+              //   const cardDict = {};
+              //   cardDict['id'] = x.id;
+              //   cardDict['title'] = x.name;
+              //   cardDict['description'] = x.desc;
+              //   cardDict['url'] = x.url;
+              //   cardDict['comments'] = x.badges.comments;
+              //   cardDict['list'] = listData[x.idList];
+              //   x.idMembers.forEach((m,index)=>{
+              //     x.idMembers[index] = memberData[m]
+              //   })
+              //   cardDict['members'] = x.idMembers.join();
+              //   x.idLabels.forEach((l,index)=>{
+              //     x.idLabels[index] = labelData[l];
+              //   })
+              //   cardDict['labels'] = x.idLabels.join(', ');
+              //   if(x.dateLastActivity){
+              //     let date = new Date(x.dateLastActivity);
+              //     date = date.toDateString();
+              //     cardDict['last activity'] = date;
+              //   }
+              //   if(x.due){
+              //     let date = new Date(x.due);
+              //     date = date.toDateString();
+              //     cardDict['due date'] = date;
+              //   }
+              //   if(x.customFieldItems.length > 0){
+              //     cardDict['priority'] = priorityData[x.customFieldItems[0].idValue]
+              //   }
+              //   cardList.push(cardDict);
+              // })
 
-              const finalList = []
-              cardList.forEach(x=>{
-                finalList.push(Object.values(x))
-              })
-              console.log(finalList)
+              // const finalList = []
+              // cardList.forEach(x=>{
+              //   finalList.push(Object.values(x))
+              // })
+              // console.log(finalList)
               
               // console.log(JSON.stringify(finalList))
               // const cs = await fetch(createSheet,{method:'POST',body:JSON.stringify(finalList),headers: {'Content-Type': 'application/json'}})
