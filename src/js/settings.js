4ce47d60ee1ba2b46847886239e915420213d01f
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-08 11:26:57 -0500

    fix get logic

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,8 +4,9 @@ const submitBtn = document.getElementById('submit')
 const idValue = document.getElementById('idValue')
 t.render(()=>{
 	return t.get('board','shared','bid')
-}).then(data=>{
-	console.log(JSON.stringify(data))
+		.then(data=>{
+			console.log(JSON.stringify(data))
+		})
 })
 
 submitBtn.addEventListener('click',()=>{
