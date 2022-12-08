eb31b64f47bff5a301b772aeae89a626d6fc2f6f
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-08 11:22:25 -0500

    add get logic to settings.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,6 +2,11 @@ var t = TrelloPowerUp.iframe();
 
 const submitBtn = document.getElementById('submit')
 const idValue = document.getElementById('idValue')
+t.render(()=>{
+	return t.get('board','shared','bid')
+}).then(data=>{
+	console.log(JSON.stringify(data))
+})
 
 submitBtn.addEventListener('click',()=>{
 	console.log(idValue.value);
