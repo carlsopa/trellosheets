53fc923e7890ed3bcb73f1c1216c49628ae4d016
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-08 11:38:40 -0500

    flushing out settings.js with set and get

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,15 +2,27 @@ var t = TrelloPowerUp.iframe();
 
 const submitBtn = document.getElementById('submit')
 const idValue = document.getElementById('idValue')
+let set = false;
 t.render(()=>{
 	const aa = t.get('board','shared','bid')
 		.then(data=>{
-			return JSON.stringify(data)
+			let d = JSON.stringify(data)
+			if(d){
+				console.log('found')
+				set = true;
+			} else {
+				console.log('missing')
+				set = false;
+			}
 		})
 		console.log(aa)
 })
 
 submitBtn.addEventListener('click',()=>{
-	console.log(idValue.value);
+	if(!set){
+		t.set('board','shared','bid',idValue.value)
+	} else{
+		console.log(idValue.value)
+	}
 	t.closePopup();
 })
