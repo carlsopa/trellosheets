cc3f162fb53a5ccef5bdefa574089e4c262f13a5
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-08 10:53:22 -0500

    adjust logic on integrator

--- src/js/integrator.js
+++ src/js/integrator.js
@@ -1,6 +1,6 @@
 var t = TrelloPowerUp.iframe();
 console.log('data')
-const elm = document.getElementById('name');
+const elm = document.getElementById('subbtn');
 console.log(elm)
 // window.subbtn.addEventListener('click',(event)=>{
 // 	console.log('paul is my name')
