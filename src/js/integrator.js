c22efbf0e40beb8345f21c6ac0f0031ee58d671b
Paul R. Carlson <carlsonpar@gmail.com>
2022-12-08 10:55:43 -0500

    trying to target class names

--- src/js/integrator.js
+++ src/js/integrator.js
@@ -1,7 +1,9 @@
 var t = TrelloPowerUp.iframe();
 console.log('data')
-const elm = document.getElementById('subbtn');
+const elm = document.getElementById('name');
 console.log(elm)
+console.log(window.subbtn);
+console.log(window)
 // window.subbtn.addEventListener('click',(event)=>{
 // 	console.log('paul is my name')
 // })
